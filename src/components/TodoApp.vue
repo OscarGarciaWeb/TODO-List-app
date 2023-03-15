ad828f729e0e4100d214d31772ad83d8319d85bc
SIGMARILION <THESIGMARILION@HOTMAIL.COM>
2023-03-15 13:09:05 +0100

    Event click button displays input and status dropdown

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -11,32 +11,61 @@
      <button @click="submitTask" class="btn btn-warning">Enviar</button> 
   </div>
 
+  <!-- <div>
+    <v-data-table headers="headers" items="tasks">
+      <template v-slot:item.actions="{ item }">
+        <v-icon
+          small
+          class="mr-2"
+          @click="editTask(item)"
+        >
+          mdi-pencil
+        </v-icon>
+        <v-icon
+          small
+          @click="deleteTask(item)"
+        >
+          mdi-delete
+        </v-icon>
+      </template>
+    </v-data-table>
+  </div>  -->
+
+
+  
+
   <div class="d-flex p-2 mt-2">
     <table class="table table-bordered">
       <thead>
         <tr>
           <th scope="col">Task</th>
           <th scope="col">Status</th>
-          <th scope="col" class="text-center">#</th>
-          <th scope="col" class="text-center">#</th>
+          <th scope="col" class="text-center">Edit</th>
+          <th scope="col" class="text-center">Delete</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(task, index) in tasks" :key="index">
           <td style="width: 350px;">
-            <span :class="{'finished': task.status === 'Finished'}">{{task.name}}</span>
+          <input :class="{'finished': task.status === 'Finished'}" v-model="task.name" type="text" class="form-control ml-2 mr-4">
           </td>
-          <td style="width: 200px;">
-            <span @click="changeStatus(index)" class="pointer" 
-              :class="
-                {
-                  'todo': task.status === 'To-do',
-                  'inprogress': task.status === 'In-Progress',
-                  'end': task.status === 'Finished'                  
-                }"
-              > 
-                {{task.status}} 
-              </span>
+          <td>
+              <div class="dropdown">
+                <button class="btn dropdown-toggle" :id=index.toString() @click="openMenu(index)" 
+                  :class="{ 
+                    'todo': tasks[index].status === 'To-do',
+                    'inprogress': tasks[index].status === 'In-Progress',
+                    'end': tasks[index].status === 'Finished'                  
+                  }"
+                >
+                  {{ task.status }}
+                </button>
+                <ul class="dropdown-menu" :class="{ show: menu === index }">
+                  <li><a class="dropdown-item" @click="changeStatus(index, value = 'todo')">To-do</a></li>
+                  <li><a class="dropdown-item" @click="changeStatus(index, value = 'inprogress')">In-progress</a></li>
+                  <li><a class="dropdown-item" @click="changeStatus(index, value = 'finished')">Finished</a></li>
+                </ul>
+              </div>
           </td>
           <td>
             <div class="text-center" @click="editTask(index)">
@@ -56,19 +85,21 @@
 <script lang="ts">
   export default{
     data(){
-      return{
+      return {
         task:'',
+        menu: -1,
+        value: '',
         editItem:null,
         avaliableStatus: ['To-do','In-Progress','Finished'],
-
+        showMenu: false,
         tasks: 
         [
           {
-          name: 'comprar platanos',
+          name: 'Comprar platanos',
           status: 'To-do'
           },
           {
-          name: 'comprar Pan',
+          name: 'Comprar Pan',
           status: 'In-Progress'
           },
         ]
@@ -77,7 +108,6 @@
     methods: {
       submitTask(){
         if (this.task.length === 0) return;
-
         if (this.editItem === null) {
           this.tasks.push({
             name: this.task,
@@ -88,8 +118,6 @@
           this.tasks[this.editItem].name = this.task;
           this.editItem = null;
         }
-
-
         this.task = '';
       },
       deleteTask(index: any) {
@@ -99,10 +127,26 @@
         this.task = this.tasks[index].name;
         this.editItem = index;
       },
-      changeStatus(index: any){
-        let newIndex = this.avaliableStatus.indexOf(this.tasks[index].status);
-        if (++newIndex >2) newIndex = 0;
-        this.tasks[index].status = this.avaliableStatus[newIndex];
+      changeStatus(index: any, value: string){
+        if (value === 'todo') {
+          this.tasks[index].status = 'To-do';
+          this.menu = -1;
+        } else if (value === 'inprogress') {
+          this.tasks[index].status = 'In-Progress';
+          this.menu = -1;
+        }
+        else{
+          this.tasks[index].status = 'Finished';
+          this.menu = -1;
+        }
+      },   
+      openMenu(index: any) {
+        if (index === this.menu) {
+          this.menu = -1;
+        }
+        else{
+          this.menu = index;
+        }
       },
     }
   }
